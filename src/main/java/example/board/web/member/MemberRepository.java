92c9a27716d3ec075d42ed924204ef982e109c59
IsaacHana <147087498+IsaacHana@users.noreply.github.com>
2024-08-14 12:10:37 +0900

    feat: 멤버 컨트롤러 추가

---
+++ src/main/java/example/board/web/member/MemberRepository.java
@@ -0,0 +1,12 @@
+package example.board.web.member;
+
+import example.board.domain.member.Member;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+}
