91ebe6c0bca5038069a9e2ab4698af21d0bf3fd8
IsaacHana <147087498+IsaacHana@users.noreply.github.com>
2024-08-14 12:14:27 +0900

    feat: 로그인 페이지 컨트롤러

--- src/main/java/example/board/web/login/LoginController.java
+++ src/main/java/example/board/web/login/LoginController.java
@@ -1,8 +1,13 @@
 package example.board.web.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
 
+    @GetMapping("/login")
+    public String login() {
+        return "login/loginForm";
+    }
 }
